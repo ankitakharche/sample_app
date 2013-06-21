e204c1699c66840f97f43467b5aeb009386a55ac
atish <atish.lewate@kunalinfotech.net>
2013-06-21 17:12:43 +0530

    Added layouts with links

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -22,4 +22,10 @@ describe PagesController do
       response.should be_success
     end
   end
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+  end
 end
