96279cfe0ab617c6e552fc6f80bb1765e33b5a2c
atish <atish.lewate@kunalinfotech.net>
2013-07-01 11:56:42 +0530

    Handling an invalid signin

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,22 +2,32 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
-	it "should have a Home page at '/'" do
-		get '/'
+	it "should have a Home page at '/home'" do
+		get '/home'
 		response.should have_selector('title', :content => "Home")
     end
 
-    it "should have a contact page at '/'" do
+    it "should have a contact page at '/contact'" do
     	get '/contact'
     	response.should have_selector('title', :content => "Contact")
     end
 
-    it "should have a Signup page at '/'" do
+    it "should have a contact page at '/about'" do
+        get '/about'
+        response.should have_selector('title', :content => "About")
+    end
+
+    it "should have a contact page at '/help'" do
+        get '/help'
+        response.should have_selector('title', :content => "Help")
+    end
+
+    it "should have a Signup page at '/signup'" do
         get '/signup'
         response.should have_selector('title', :content => "Sign up")
     end
 
-    it "should have a Signin page at '/'" do
+    it "should have a Signin page at '/signin'" do
         get '/signin'
         response.should have_selector('title', :content => "Sign in")
     end
