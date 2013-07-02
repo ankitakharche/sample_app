7ae6dc59a60b66c7b46711cd20230869abb574cc
atish <atish.lewate@kunalinfotech.net>
2013-07-02 18:00:12 +0530

    friendly forwarding and correct user requirment

--- spec/controllers/factories.rb
+++ spec/controllers/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user| do 
-user.name                  "oliver"
-user.email                 "oliver@email.com"
-user.password              "foobar"
-user.password_confirmation "foobar"
+  user.name                  "oliver"
+  user.email                 "oliver@email.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
 end
